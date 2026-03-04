a148038cca8bb8af1b3cd1e72f17ee55639d4ac5
Caio Richard <crkautza@gmail.com>
2026-03-04 14:44:36 -0300

    Funcionalidade de Desfazer e Adicionar palavra, criada!

--- script.js
+++ script.js
@@ -1,15 +1,52 @@
+//Elements
 let TextArea = document.getElementsByTagName('textarea')[0];
 let AddButton = document.getElementById('add');
 let UndoButton = document.getElementById('undo');
 let RedoButton = document.getElementById('redo');
 
-AddButton.addEventListener('click', () =>{
+//Arrays
+let history = [''];
+let undo = [''];
+let redo = [''];
+//let cut = [''];
+
+//Event Listeners
+TextArea.addEventListener('input', textArea);
+AddButton.addEventListener('click', addFunction);
+UndoButton.addEventListener('click', undoFunction);
+RedoButton.addEventListener('click', redoFunction);
+
+//Functions
+function addFunction(){
     TextArea.value += 'Palavra';
-    console.log('Botão Add clicado!', TextArea.value);
-});
-UndoButton.addEventListener('click', () =>{
-    console.log('Botão Undo clicado!');
-});
-RedoButton.addEventListener('click', () =>{
-    console.log('Botão Redo clicado!');
-});
+    varPush();
+}
+
+function undoFunction(){
+    undo.pop();
+    print();
+    undo.forEach(elemento => {
+        TextArea.value = elemento;
+    });;
+    history.push(TextArea.value);
+    redo.push(TextArea.value);
+}
+
+function varPush(){
+    history.push(TextArea.value);
+    undo.push(TextArea.value);
+    redo.push(TextArea.value);
+}
+
+function print(){
+    console.log('------------');
+    console.log('Undo:', undo);
+    console.log('History:', history);
+    console.log('Redo', redo);
+    //console.log('Cut', cut);
+}
+
+function textArea(){
+    varPush();
+    print();
+}
