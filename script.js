9365b2415b4e2d64e2a039c26a3f15fcb6f60cba
Caio Richard <crkautza@gmail.com>
2026-03-04 20:05:29 -0300

    Ajustes de erro no Undo!

--- script.js
+++ script.js
@@ -2,19 +2,15 @@
 let TextArea = document.getElementsByTagName('textarea')[0];
 let AddButton = document.getElementById('add');
 let UndoButton = document.getElementById('undo');
-let RedoButton = document.getElementById('redo');
 
 //Arrays
-let history = [''];
+let historico = [''];
 let undo = [''];
-let redo = [''];
-//let cut = [''];
 
 //Event Listeners
 TextArea.addEventListener('input', textArea);
 AddButton.addEventListener('click', addFunction);
 UndoButton.addEventListener('click', undoFunction);
-RedoButton.addEventListener('click', redoFunction);
 
 //Functions
 function addFunction(){
@@ -23,30 +19,34 @@ function addFunction(){
 }
 
 function undoFunction(){
+    if(TextArea.value){
     undo.pop();
     print();
     undo.forEach(elemento => {
         TextArea.value = elemento;
     });;
-    history.push(TextArea.value);
-    redo.push(TextArea.value);
+    historico.push(TextArea.value);
+    }
+    //redo.push(TextArea.value);
 }
 
 function varPush(){
-    history.push(TextArea.value);
+    historico.push(TextArea.value);
     undo.push(TextArea.value);
-    redo.push(TextArea.value);
+    //redo.push(TextArea.value);
 }
 
 function print(){
     console.log('------------');
     console.log('Undo:', undo);
-    console.log('History:', history);
-    console.log('Redo', redo);
-    //console.log('Cut', cut);
+    console.log('Histórico:', historico);
 }
 
 function textArea(){
+    if (historico.length >= 20 && undo.length >= 20) {
+    historico.splice(0, 17);
+    undo.splice(0, 17);
+    }
     varPush();
     print();
 }
